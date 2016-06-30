aa4eff0d85d677016bb1cf5cb37135e1f03d1715
Junda <junda@just2us.com>
2016-06-30 11:58:24 +0800

    Remove use of -e for `echo` Closed #1

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo -e "#!/usr/bin/swift\n$(cat wwdc-dl-Playground.playground/Contents.swift)" > wwdc-dl-tmp.swift
+rm -f wwdc-dl-tmp.swift
+echo "#!/usr/bin/swift" > wwdc-dl-tmp.swift
+cat wwdc-dl-Playground.playground/Contents.swift >> wwdc-dl-tmp.swift
 xcrun -sdk macosx swiftc wwdc-dl-tmp.swift -o wwdc-dl
